79213a5e124750a02e1b7a5761de166fbb9afc95
André J. Kelling <kontakt@andrekelling.de>
2026-06-29 21:08:19 +0200

    COMPOSER setup for moving code into a class

---
+++ classes/Search.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace BlockFinder;
+
+final class Search
+{
+  public static function find(): array
+  {
+    $type = get('type');
+
+    if (!$type) {
+      return [];
+    }
+    $kirby = kirby();
+    $results = [];
+    $fieldName = option('andrekelling.kirby-block-finder.fieldName');
+
+    foreach (site()->index() as $page) {
+      foreach($kirby->languages() as $lang) {
+        $langCode = $lang->code();
+
+        if (!$page->content($langCode)->exists()) {
+          continue;
+        }
+
+        $blocksField = $page->content()->get($fieldName);
+
+        if ($blocksField->isEmpty()) {
+          continue;
+        }
+
+        $count = 0;
+        $allBocksOnPage = $blocksField->value();
+        $allBocksOnPageArr = json_decode($allBocksOnPage);
+        foreach ($allBocksOnPageArr as $block) {
+          if ($block->type === $type) {
+            $count++;
+          }
+        }
+        if ($count > 0) {
+          $results[] = [
+            'lang' => $langCode,
+            'pageId' => $page->id(),
+            'title' => $page->title()->value(),
+            'count' => $count,
+            'panelUrl' => $page->panel()->url() . '?language=' . $langCode
+          ];
+        }
+      }
+    }
+
+    return $results;
+  }
+
+}
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+use BlockFinder\Search;
+
+@include_once __DIR__.'/vendor/autoload.php';
+
 Kirby::plugin('andrekelling/kirby-block-finder', [
   'options' => [
     'fieldName' => 'blocks'
@@ -48,51 +52,7 @@ Kirby::plugin('andrekelling/kirby-block-finder', [
         'pattern' => 'block-finder/search',
         'method'  => 'GET',
         'action'  => function () {
-
-          $type = get('type');
-
-          if (!$type) {
-            return [];
-          }
-          $kirby = kirby();
-          $results = [];
-          $fieldName = option('andrekelling.kirby-block-finder.fieldName');
-
-          foreach (site()->index() as $page) {
-            foreach($kirby->languages() as $lang) {
-              $langCode = $lang->code();
-
-              if (!$page->content($langCode)->exists()) {
-                continue;
-              }
-
-              $blocksField = $page->content()->get($fieldName);
-
-              if ($blocksField->isEmpty()) {
-                continue;
-              }
-
-              $count = 0;
-              $allBocksOnPage = $blocksField->value();
-              $allBocksOnPageArr = json_decode($allBocksOnPage);
-              foreach ($allBocksOnPageArr as $block) {
-                if ($block->type === $type) {
-                  $count++;
-                }
-              }
-              if ($count > 0) {
-                $results[] = [
-                  'lang' => $langCode,
-                  'pageId' => $page->id(),
-                  'title' => $page->title()->value(),
-                  'count' => $count,
-                  'panelUrl' => $page->panel()->url() . '?language=' . $langCode
-                ];
-              }
-            }
-          }
-
-          return $results;
+          return Search::find();
         }
       ]
     ]
